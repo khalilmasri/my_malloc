690de471059a35233c429ed5d58ac29d11f36a64
khalilmasri <khalil.1996km@gmail.com>
2021-12-07 19:18:25 +0200

    Utility

--- src/utility.c
+++ src/utility.c
@@ -4,73 +4,31 @@ size_t align(size_t size){
     return (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1));
 }
 
-void *mem_block(block_t *ptr){
-    return ((void *)((unsigned long)ptr + sizeof(block_t)));
+size_t chunk_align(size_t size){
+    return (((size)+(CHUNK_SIZE-1)) & ~(CHUNK_SIZE-1));
 }
 
-void remove_free_list(block_t *ptr){
-
-    if(!ptr->prev){
-        if(ptr->next){
-            head = ptr->next;
-        }else{
-            head = NULL;
-        }
-    }else{
-        ptr->prev->next = ptr->next;
-    }
-
-    if(ptr->next)
-        ptr->next->prev = ptr->prev;
+void memory_map(size_t size){
+    mem = mmap(0, chunk_align(size), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1 , 0);
+    available_size = chunk_align(size);
+    heap.block_size = 1;
+    heap.blocks[0].address = mem;
+    heap.blocks[0].allocated = 0;
+    heap.blocks[0].size = available_size;
 }
 
-void add_to_free_list(block_t *ptr){
+heap_t add_blocks(size_t size){
 
-    ptr->next = NULL;
-    ptr->prev = NULL;
+    heap.blocks[heap.block_size-1].address = mem;
+    heap.blocks[heap.block_size-1].size = size;
+    heap.blocks[heap.block_size-1].allocated = 1;
 
-    if(!head || (unsigned long)head > (unsigned long)ptr){
-        if(head)
-            head->prev = ptr;
+    mem += size;
+    available_size -= size;
 
-        ptr->next = head;
-        head = ptr;
-    }else{
-        block_t *temp = head;
-        while(temp->next && (unsigned long)temp->next < (unsigned long)ptr){
-            temp = temp->next;
-        }
+    heap.blocks[heap.block_size].address = mem;
+    heap.blocks[heap.block_size].size = available_size;
+    heap.block_size += 1;
 
-        ptr->next = temp->next;
-        temp->next = ptr;
-    }
-}
-
-void *split_mem(block_t *ptr, size_t size){
-
-    void *mem = mem_block(ptr);
-    block_t *new = (block_t*)((unsigned long)mem + size);
-
-    new->size = ptr->size - (size+sizeof(block_t));
-    ptr->size = size;
-    return new;
-}
-
-void *allocate_mem(block_t *ptr, size_t alloc_size, size_t passed_size){
-
-    ptr = mmap(0, alloc_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    if(!ptr){
-        printf("Coudln't allocate in memory %ld.\n", passed_size);
-        return NULL;
-    }
-    
-    ptr->next = NULL;
-    ptr->prev = NULL;
-    ptr->size = passed_size;
-    if(alloc_size > passed_size + sizeof(block_t)){
-        block_t *new = split_mem(ptr, passed_size);
-        add_to_free_list(new);
-    }
-
-    return ptr;
+    return heap;
 }
