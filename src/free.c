b357f4f03a1607e11d34744844188d3d147f8b0d
khalilmasri <khalil.1996km@gmail.com>
2021-12-07 19:18:29 +0200

    _free

--- src/free.c
+++ src/free.c
@@ -1,36 +1,17 @@
 #include "../include/include.h" 
 
-void merge_free(){
+void **free_ = NULL;
 
-    block_t *temp = head;
-
-    unsigned long curr, next;
-    
-    while(temp->next){
-        curr = (unsigned long)temp + temp->size + sizeof(block_t);
-        next = (unsigned long)temp->next;
-            printf("cur %ld next %ld\n", curr, next);
-
-        if(next == curr){
-            temp->size += temp->next->size + sizeof(block_t);
-            temp->next = temp->next->next;
-            if(temp->next){
-                temp->next->prev = temp;
-            }else{
-                break;
-            }
-        }
-        temp = temp->next;
-    }
-}
-
-void free_(void **ptr){
-
-    *ptr = ((void*)(unsigned long)ptr - sizeof(block_t));
-    add_to_free_list(*ptr);
-    merge_free();
+void unmap(void **ptr){
 }
 
 void _my_free(void *ptr){
-    free_(ptr);
+
+    for(size_t i = 0; i < heap.block_size; i++){
+        if(heap.blocks[i].address == ptr){
+            heap.blocks[i].allocated = 0;
+            printf("Ptr %p\n", ptr);
+            break;
+        }
+    }
 }
