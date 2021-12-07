f2b3800f83ad363cf10a9cc98300b7c9fe8aa448
khalilmasri <khalil.1996km@gmail.com>
2021-12-07 19:18:38 +0200

    _malloc

--- src/malloc.c
+++ src/malloc.c
@@ -1,38 +1,70 @@
 #include "../include/include.h"
 
-struct block *head = NULL;
+heap_t heap;
+void *mem = NULL;
+size_t available_size = 0;
+
+void print_heap(int index, size_t size){
+
+    printf("Address: %p heap size: %ld heap allocated %d trying size : %ld\n", heap.blocks[index].address, heap.blocks[index].size, heap.blocks[index].allocated, size);
+}
+
+void split_heap(int index, size_t size){
+
+    heap.block_size += 1;
+
+    int i;
+
+    for(i = heap.block_size -1; i > index + 1; i--){
+        heap.blocks[i] = heap.blocks[i-1];
+    }
+
+    heap.blocks[i].address = heap.blocks[index].address + (heap.blocks[index].size - size);
+    heap.blocks[i].size = (heap.blocks[index].size - size);
+    heap.blocks[i].allocated = 0;
+
+    i -= 1;
+    heap.blocks[i].address = heap.blocks[i+1].address - heap.blocks[i+1].size;
+    heap.blocks[i].size = size;
+    heap.blocks[i].allocated = 1;
+}
+
+void *search_available_blocks(size_t size){
+
+    for(int i = 0; i < heap.block_size-1; i++){
+        if(heap.blocks[i].allocated == 0){
+            if(heap.blocks[i].size == size){
+                heap.blocks[i].allocated = 1;
+                return heap.blocks[i].address;
+                break;
+            }else if(heap.blocks[i].size > size){
+                printf("Here\n");
+                split_heap(i, size);
+                return heap.blocks[i].address;
+                break;
+            }
+        }
+    }
+
+    return NULL;
+}
 
 void *_my_malloc(size_t size){
 
     if(size <= 0)
         return NULL;
 
-    void *mem;
-    block_t *temp, *new;
-    size_t alloc_size = align(size) + sizeof(block_t);
+    size_t allocate_size = align(size);
 
-    temp = head;
+    void *new;
 
-    while(temp){
-        if(temp->size >= size){
-           mem = mem_block(temp);
-           remove_free_list(temp);
-           if(temp->size == size){
-               return mem;
-           }
+    if(available_size < allocate_size)
+        memory_map(allocate_size);
 
-           new = split_mem(temp, size);
-           add_to_free_list(new);
-           return mem;
-        }else{
-            temp = temp->next;
-        }
+    if(!( new = search_available_blocks(allocate_size))){
+        heap = add_blocks(allocate_size);
+        return heap.blocks[heap.block_size-1].address;
     }
 
-    // We couldn't find free blocks
-    if(!(temp = allocate_mem(temp,alloc_size,size)))
-        return NULL;
-
-    return mem_block(temp);
+    return new;
 }
-
