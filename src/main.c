146a41f72b4f9c488e3a639debcdab603bfc1781
khalilmasri <khalil.1996km@gmail.com>
2021-12-08 14:08:47 +0200

    main

--- src/main.c
+++ src/main.c
@@ -17,17 +17,7 @@
  */
 #include "../include/include.h" 
 
-
 int main(){
-
-    /* char *src = "Hello"; */
-    /* char *dest = _my_malloc(100); */
-    /* strcpy(dest, src); */
-
-    /* printf("dest: %s size: %ld address: %p \n", dest, strlen(dest), dest); */
-
-
-    /* /1* _my_free(dest); *1/ */
 
     char *src = _my_malloc(100);
     char *dest = _my_malloc(100);
@@ -38,24 +28,39 @@ int main(){
     strcpy(dest, "helo");
     strcpy(t, "hlo");
     strcpy(r, "o");
+
+    char *c = _my_realloc(dest, 3);
+    printf("c: %s c: %ld address: %p \n", c, strlen(c), c);
+    _my_free(dest);
+    /* _my_free(dest); */
+    /* printf("dest: %s dest: %ld address: %p \n", dest, strlen(dest), dest); */
+
+    char *s = _my_malloc(20);
+    strcpy(s, "mama");
+
+    char *k = _my_malloc(30);
+    strcpy(k, "ho");
+
+    char *f = _my_malloc(50);
+    strcpy(f, "bk");
+
+    char *n = _my_malloc(9);
+    strcpy(n, "ma");
+
+    char *m = _my_calloc(30, sizeof(char));
     printf("src: %s src: %ld address: %p \n", src, strlen(src), src);
     printf("r: %s r: %ld address: %p \n", r, strlen(r), r);
     printf("t: %s t: %ld address: %p \n", t, strlen(t), t);
-    printf("dest: %s dest: %ld address: %p \n", dest, strlen(dest), dest);
-
-
-    _my_free(dest);
-    printf("dest: %s dest: %ld address: %p \n", dest, strlen(dest), dest);
-    char *s = _my_malloc(20);
-    strcpy(s, "mama");
+    printf("k: %s k: %ld address: %p \n", k, strlen(k), k);
     printf("s: %s s: %ld address: %p \n", s, strlen(s), s);
-    printf("src: %s src: %ld address: %p \n", src, strlen(src), src);
-    printf("dest: %s dest: %ld address: %p \n", dest, strlen(dest), dest);
-    printf("t: %s t: %ld address: %p \n", t, strlen(t), t);
-    char *k = _my_malloc(30);
-    strcpy(k, "ho");
-    printf("s: %s s: %ld address: %p \n", k, strlen(k), k);
-    /* printf("dest: %s dest: %ld address: %p \n", dest, strlen(dest), dest); */
+    printf("f: %s s: %ld address: %p \n", f, strlen(f), f);
+    printf("m: %s m: %ld address: %p \n", m, strlen(m), m);
+    printf("n: %s n: %ld address: %p \n", n, strlen(n), n);
+
+    printf("\n");
+    for(size_t i = 0 ; i < heap.block_size; i++){
+        printf("{size: %ld Address: %ld allocated: %d}\n", heap.blocks[i].size, (long)heap.blocks[i].address, heap.blocks[i].allocated);
+    }
 
     return 0;
     /* _my_free(dest); */
