82eb3c27b27d5aa7c495108dd32561e87534ceef
khalilmasri <khalil.1996km@gmail.com>
2021-12-08 14:08:42 +0200

    _free

--- src/free.c
+++ src/free.c
@@ -1,20 +1,30 @@
 #include "../include/include.h" 
 
-void **free_ = NULL;
+void merge_empty_blocks(){
 
-void unmap(void **ptr){
-    *ptr = NULL;
+    for(size_t i = 0; i < heap.block_size-1; i++){
+        if(heap.blocks[i].allocated == 0 && heap.blocks[i+1].allocated == 0){
+            heap.blocks[i].size += heap.blocks[i+1].size;
+            shift_back(i);
+            heap.block_size -= 1;
+        }
+    }
+
 }
 
-void _my_free(void *ptr){
+void my_free(void *ptr){
+
+    if(ptr == NULL)
+        printf("Trying to free an invalid pointer.\n");
 
     for(size_t i = 0; i < heap.block_size; i++){
         if(heap.blocks[i].address == ptr){
             heap.blocks[i].allocated = 0;
             heap.blocks[i].address = NULL;
-            unmap(ptr);
-            printf("Ptr %p\n", ptr);
-            break;
+            merge_empty_blocks();
+            return;
         }
     }
+
+    printf("Trying to free an invalid pointer.\n");
 }
