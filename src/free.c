9a919901f1d933cc91af52a9be9de8adbf938f2c
khalilmasri <khalil.1996km@gmail.com>
2021-12-08 11:42:38 +0200

    _free

--- src/free.c
+++ src/free.c
@@ -3,6 +3,7 @@
 void **free_ = NULL;
 
 void unmap(void **ptr){
+    *ptr = NULL;
 }
 
 void _my_free(void *ptr){
@@ -10,6 +11,8 @@ void _my_free(void *ptr){
     for(size_t i = 0; i < heap.block_size; i++){
         if(heap.blocks[i].address == ptr){
             heap.blocks[i].allocated = 0;
+            heap.blocks[i].address = NULL;
+            unmap(ptr);
             printf("Ptr %p\n", ptr);
             break;
         }
