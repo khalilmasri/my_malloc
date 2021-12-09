04b328691edc1323b6acb9552c66007f1ecb81ac
khalilmasri <khalil.1996km@gmail.com>
2021-12-09 13:18:34 +0200

    _realloc

--- src/realloc.c
+++ src/realloc.c
@@ -1,5 +1,15 @@
 #include "../include/include.h"
 
+void _my_memcpy(void *dest, void *src, size_t size){
+
+    char* cdest = (char*)dest;
+    char* csrc = (char*)src;
+
+    for(size_t i = 0; i < size; i++){
+        cdest[i] = csrc[i];
+    }
+
+}
 void *_my_realloc(void *ptr, size_t size){
 
     if(size <= 0 || size > available_size)
@@ -7,7 +17,13 @@ void *_my_realloc(void *ptr, size_t size){
 
     void *new = _my_malloc(size);
 
-    new = ptr;
+    size_t i = 0;
+    for(i = 0 ; i < heap.block_size; i++){
+        if(ptr == heap.blocks[i].address)
+            break;
+    }
+
+    _my_memcpy(new, ptr, align(size)); 
      
     _my_free(ptr);
 
