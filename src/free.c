95a8882e0e00543421b2d4038858d810df1349dd
khalilmasri <khalil.1996km@gmail.com>
2021-12-09 13:18:23 +0200

    _free

--- src/free.c
+++ src/free.c
@@ -20,7 +20,6 @@ void my_free(void *ptr){
     for(size_t i = 0; i < heap.block_size; i++){
         if(heap.blocks[i].address == ptr){
             heap.blocks[i].allocated = 0;
-            heap.blocks[i].address = NULL;
             merge_empty_blocks();
             return;
         }
